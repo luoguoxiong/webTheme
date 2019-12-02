315fa2d84e4d5af0c9bb5d21a3cb85b28558a3e6
罗国雄 <luoguoxiong@ipin.com>
2019-12-02 17:30:30 +0800

    test

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+// http://ipin.oss.com/set?bg=orange&color=white&oem=oem4
+// http://ipin.oem2.com/index.html
 const koa = require("koa");
 const webpack = require("webpack");
 const staticCache = require("koa-static-cache");
+const Router = require("koa-router");
 const cssConfig = require("./webpack.config");
 const path = require("path");
 const fs = require("fs");
-const open = require("open");
-const app = new koa();
 
-const buildCss = (bg, color) => {
+const app = new koa();
+const router = new Router();
+
+router.get("/set", async ctx => {
+  const { bg, color, oem } = ctx.query;
+  await buildCss(bg, color, oem);
+  ctx.body = { cdn: `http://localhost:3000/${oem}.css` };
+});
+const buildCss = (bg, color, oem) => {
   return new Promise(res => {
     fs.writeFile(
       path.resolve(__dirname, "./css/var.less"),
@@ -16,32 +25,27 @@ const buildCss = (bg, color) => {
         if (err) {
           res(false);
         } else {
-          webpack(cssConfig, () => {
-            const file = fs.readFileSync(
-              path.resolve(__dirname, "./build/index.css"),
-              "utf8"
-            );
-            res(file);
+          const config = cssConfig(oem);
+          webpack(config, (err, stats) => {
+            if (err) {
+              res(false);
+            } else {
+              res(true);
+              console.log("done");
+            }
           });
+          // todo cdn上传
         }
       }
     );
   });
 };
-
+app.use(router.routes());
 app.use(
   staticCache(path.resolve(__dirname, "./build"), {
     maxAge: 0
   })
 );
-app.use(async (ctx, next) => {
-  if (!ctx.path.includes("css")) {
-    const { bg, color } = ctx.query;
-    await buildCss(bg, color);
-    open("http://localhost:3000/index.html");
-    ctx.body = { cdn: "http://localhost:3000/index.css" };
-  }
-});
 app.listen(3000, function() {
   console.log("server is run in 3000");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-  entry: "./css/index.js",
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "./build")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(css|less)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
-      }
+module.exports = cssName => {
+  return {
+    entry: "./css/index.js",
+    mode: "production",
+    output: {
+      path: path.resolve(__dirname, "./build")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(css|less)$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `${cssName}.css`
+      })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `index.css`,
-      chunkFilename: `index.css`,
-      ignoreOrder: false
-    })
-  ]
+  };
 };
